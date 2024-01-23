62d35c980435dcd3be3795e49773a48f059bdecc
Johnny <johnkcli@hotmail.com>
2024-01-23 20:16:54 +0000

    added assertion for filtering irrelevant cities from direct destinations

--- functional_tests/test_direct_destinations.py
+++ functional_tests/test_direct_destinations.py
@@ -13,6 +13,22 @@ class DirectDestinationsTest(TestCase):
             response.data,
             key=lambda destination_city: destination_city["travel_distance_km"],
         )
+        assert response.data[0] == {
+            "type": "location",
+            "subtype": "city",
+            "name": "MANCHESTER",
+            "iataCode": "MAN",
+            "geoCode": {"latitude": 53.35362, "longitude": -2.275},
+            "address": {
+                "countryName": "UNITED KINGDOM",
+                "countryCode": "GB",
+                "regionCode": "EUROP",
+            },
+            "timeZone": {"offSet": "+00:00"},
+            "metrics": {"relevance": 4},
+            "travel_distance_km": 251.1967389376186,
+            "travel_distance_miles": 156.08641715979837,
+        }
         assert response.data[-1] == {
             "type": "location",
             "subtype": "city",
