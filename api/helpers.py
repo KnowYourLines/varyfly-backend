9450648290704c867ae515acc39f4fbb0e1d084b
Johnny <johnkcli@hotmail.com>
2024-01-28 01:45:44 +0000

    find direct destinations for origin and destination in same endpoint

--- api/helpers.py
+++ api/helpers.py
@@ -1,21 +1,29 @@
+import logging
 import os
 
 import requests
+from geopy import distance
 
 
 def access_token_and_type():
-    response = requests.post(
-        f"https://{os.environ.get('AMADEUS_BASE_URL')}/v1/security/oauth2/token",
-        data={
-            "grant_type": "client_credentials",
-            "client_id": os.environ.get("AMADEUS_API_KEY"),
-            "client_secret": os.environ.get("AMADEUS_API_SECRET"),
-        },
-    )
-    response = response.json()
-    access_token = response["access_token"]
-    token_type = response["token_type"]
-    return token_type, access_token
+    try:
+        response = requests.post(
+            f"https://{os.environ.get('AMADEUS_BASE_URL')}/v1/security/oauth2/token",
+            data={
+                "grant_type": "client_credentials",
+                "client_id": os.environ.get("AMADEUS_API_KEY"),
+                "client_secret": os.environ.get("AMADEUS_API_SECRET"),
+            },
+        )
+        response.raise_for_status()
+        response = response.json()
+        access_token = response["access_token"]
+        token_type = response["token_type"]
+        return token_type, access_token
+    except requests.HTTPError as exc:
+        logging.error(
+            f"Error response {exc.response.status_code} while requesting {exc.request.url}: {exc.response.text}"
+        )
 
 
 def get_city_details(
@@ -24,16 +32,101 @@ def get_city_details(
     token_type,
     access_token,
 ):
-    response = requests.get(
-        f"https://{os.environ.get('AMADEUS_BASE_URL')}/v1/reference-data/locations",
-        params={
-            "keyword": city_iata,
-            "countryCode": country_code,
-            "subType": "CITY",
-        },
-        headers={"Authorization": f"{token_type} {access_token}"},
-    )
-    response.raise_for_status()
-    city_data = response.json().get("data", [])
-    city = next(city for city in city_data if city["iataCode"] == city_iata)
-    return city
+    try:
+        response = requests.get(
+            f"https://{os.environ.get('AMADEUS_BASE_URL')}/v1/reference-data/locations",
+            params={
+                "keyword": city_iata,
+                "countryCode": country_code,
+                "subType": "CITY",
+            },
+            headers={"Authorization": f"{token_type} {access_token}"},
+        )
+        response.raise_for_status()
+        city_data = response.json().get("data", [])
+        city = next(city for city in city_data if city["iataCode"] == city_iata)
+        return city
+    except requests.HTTPError as exc:
+        logging.error(
+            f"Error response {exc.response.status_code} while requesting {exc.request.url}: {exc.response.text}"
+        )
+
+
+def get_direct_destinations(
+    city_iata,
+    city_name,
+    country_iata,
+    token_type,
+    access_token,
+):
+    try:
+        home_city_details = get_city_details(
+            city_iata, country_iata, token_type, access_token
+        )
+        home_latitude = home_city_details["geoCode"]["latitude"]
+        home_longitude = home_city_details["geoCode"]["longitude"]
+        response = requests.get(
+            f"https://{os.environ.get('AMADEUS_BASE_URL')}/v1/reference-data/locations",
+            params={
+                "subType": "AIRPORT",
+                "keyword": city_name,
+                "countryCode": country_iata,
+            },
+            headers={"Authorization": f"{token_type} {access_token}"},
+        )
+        response.raise_for_status()
+        airports = [
+            airport["iataCode"]
+            for airport in response.json().get("data", [])
+            if airport["address"]["cityCode"] == city_iata
+        ]
+        direct_destinations = []
+        added_cities = set()
+        for airport_iata in airports:
+            response = requests.get(
+                f"https://{os.environ.get('AMADEUS_BASE_URL')}/v1/airport/direct-destinations",
+                params={
+                    "departureAirportCode": airport_iata,
+                },
+                headers={"Authorization": f"{token_type} {access_token}"},
+            )
+            response.raise_for_status()
+            destinations = response.json().get("data", [])
+            for city in destinations:
+                irrelevant = (
+                    city.get("metrics", {}).get("relevance", 0) == 0
+                    and city["address"]["countryCode"] == country_iata
+                )
+
+                if city["iataCode"] not in added_cities and not irrelevant:
+                    added_cities.add(city["iataCode"])
+                    direct_destinations.append(city)
+        for destination in direct_destinations:
+            destination_latitude = destination["geoCode"]["latitude"]
+            destination_longitude = destination["geoCode"]["longitude"]
+            travel_distance = distance.distance(
+                (home_latitude, home_longitude),
+                (destination_latitude, destination_longitude),
+            )
+            estimated_flight_speed_mph = 575
+            takeoff_landing_hrs = 5 / 6
+            estimated_flight_time = (
+                travel_distance.miles / estimated_flight_speed_mph + takeoff_landing_hrs
+            )
+            destination["estimated_flight_time_hrs"] = estimated_flight_time
+            destination[
+                "estimated_flight_time_hrs_mins"
+            ] = f"{int(estimated_flight_time // 1)}h {int(estimated_flight_time % 1 * 60)}m"
+            destination["name"] = destination["name"].title()
+            destination["state"] = destination["address"].get("stateCode")
+            destination["country"] = destination["address"]["countryName"].title()
+            del destination["timeZone"]["referenceLocalDateTime"]
+        direct_destinations = sorted(
+            direct_destinations,
+            key=lambda destination_city: destination_city["estimated_flight_time_hrs"],
+        )
+        return direct_destinations
+    except requests.HTTPError as exc:
+        logging.error(
+            f"Error response {exc.response.status_code} while requesting {exc.request.url}: {exc.response.text}"
+        )
--- api/views.py
+++ api/views.py
@@ -1,111 +1,85 @@
+import logging
 import os
 
-from geopy import distance
 import pycountry
 import requests
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.helpers import access_token_and_type, get_city_details
+from api.helpers import access_token_and_type, get_direct_destinations
 
 
 class CitySearchView(APIView):
     def get(self, request):
-        query = request.query_params.get("query")
-        token_type, access_token = access_token_and_type()
-        response = requests.get(
-            f"https://{os.environ.get('AMADEUS_BASE_URL')}/v1/reference-data/locations",
-            params={
-                "subType": "CITY",
-                "keyword": query,
-                "sort": "analytics.travelers.score",
-                "view": "FULL",
-            },
-            headers={"Authorization": f"{token_type} {access_token}"},
-        )
-        city_suggestions = [
-            {
-                "city_iata": city["iataCode"],
-                "city_name": city["name"].title(),
-                "country_iata": city["address"]["countryCode"],
-                "country_name": pycountry.countries.get(
-                    alpha_2=city["address"]["countryCode"]
-                ).name,
-                "state_code": city["address"].get("stateCode"),
-            }
-            for city in response.json().get("data", [])
-        ]
-        return Response(city_suggestions)
+        try:
+            query = request.query_params.get("query")
+            token_type, access_token = access_token_and_type()
+            response = requests.get(
+                f"https://{os.environ.get('AMADEUS_BASE_URL')}/v1/reference-data/locations",
+                params={
+                    "subType": "CITY",
+                    "keyword": query,
+                    "sort": "analytics.travelers.score",
+                    "view": "FULL",
+                },
+                headers={"Authorization": f"{token_type} {access_token}"},
+            )
+            response.raise_for_status()
+            city_suggestions = [
+                {
+                    "city_iata": city["iataCode"],
+                    "city_name": city["name"].title(),
+                    "country_iata": city["address"]["countryCode"],
+                    "country_name": pycountry.countries.get(
+                        alpha_2=city["address"]["countryCode"]
+                    ).name,
+                    "state_code": city["address"].get("stateCode"),
+                }
+                for city in response.json().get("data", [])
+            ]
+            return Response(city_suggestions)
+        except requests.HTTPError as exc:
+            logging.error(
+                f"Error response {exc.response.status_code} while requesting {exc.request.url}: {exc.response.text}"
+            )
 
 
 class DirectDestinationsView(APIView):
     def get(self, request):
-        city_name = request.query_params.get("city_name")
-        country_iata = request.query_params.get("country_iata")
-        city_iata = request.query_params.get("city_iata")
+        origin_city_name = request.query_params.get("origin_city_name")
+        origin_country_iata = request.query_params.get("origin_country_iata")
+        origin_city_iata = request.query_params.get("origin_city_iata")
         token_type, access_token = access_token_and_type()
-        home_city_details = get_city_details(
-            city_iata, country_iata, token_type, access_token
-        )
-        home_latitude = home_city_details["geoCode"]["latitude"]
-        home_longitude = home_city_details["geoCode"]["longitude"]
-        response = requests.get(
-            f"https://{os.environ.get('AMADEUS_BASE_URL')}/v1/reference-data/locations",
-            params={
-                "subType": "AIRPORT",
-                "keyword": city_name,
-                "countryCode": country_iata,
-            },
-            headers={"Authorization": f"{token_type} {access_token}"},
-        )
-        airports = [
-            airport["iataCode"]
-            for airport in response.json().get("data", [])
-            if airport["address"]["cityCode"] == city_iata
-        ]
-        direct_destinations = []
-        added_cities = set()
-        for airport_iata in airports:
-            response = requests.get(
-                f"https://{os.environ.get('AMADEUS_BASE_URL')}/v1/airport/direct-destinations",
-                params={
-                    "departureAirportCode": airport_iata,
-                },
-                headers={"Authorization": f"{token_type} {access_token}"},
+        origin_direct_destinations = []
+        if origin_city_iata and origin_city_name and origin_country_iata:
+            origin_direct_destinations = get_direct_destinations(
+                origin_city_iata,
+                origin_city_name,
+                origin_country_iata,
+                token_type,
+                access_token,
             )
-            destinations = response.json().get("data", [])
-            for city in destinations:
-                irrelevant = (
-                    city.get("metrics", {}).get("relevance", 0) == 0
-                    and city["address"]["countryCode"] == country_iata
-                )
-
-                if city["iataCode"] not in added_cities and not irrelevant:
-                    added_cities.add(city["iataCode"])
-                    direct_destinations.append(city)
-        for destination in direct_destinations:
-            destination_latitude = destination["geoCode"]["latitude"]
-            destination_longitude = destination["geoCode"]["longitude"]
-            travel_distance = distance.distance(
-                (home_latitude, home_longitude),
-                (destination_latitude, destination_longitude),
+        destination_city_name = request.query_params.get("destination_city_name")
+        destination_country_iata = request.query_params.get("destination_country_iata")
+        destination_city_iata = request.query_params.get("destination_city_iata")
+        destination_direct_destinations = []
+        if destination_city_iata and destination_city_name and destination_country_iata:
+            destination_direct_destinations = get_direct_destinations(
+                destination_city_iata,
+                destination_city_name,
+                destination_country_iata,
+                token_type,
+                access_token,
             )
-            estimated_flight_speed_mph = 575
-            takeoff_landing_hrs = 5 / 6
-            estimated_flight_time = (
-                travel_distance.miles / estimated_flight_speed_mph + takeoff_landing_hrs
+        if destination_direct_destinations and origin_direct_destinations:
+            direct_destinations = [
+                destination
+                for destination in origin_direct_destinations
+                if destination in destination_direct_destinations
+            ]
+        else:
+            direct_destinations = (
+                origin_direct_destinations or destination_direct_destinations
             )
-            destination["estimated_flight_time_hrs"] = estimated_flight_time
-            destination[
-                "estimated_flight_time_hrs_mins"
-            ] = f"{int(estimated_flight_time // 1)}h {int(estimated_flight_time % 1 * 60)}m"
-            destination["name"] = destination["name"].title()
-            destination["state"] = destination["address"].get("stateCode")
-            destination["country"] = destination["address"]["countryName"].title()
-            del destination["timeZone"]["referenceLocalDateTime"]
-        direct_destinations = sorted(
-            direct_destinations,
-            key=lambda destination_city: destination_city["estimated_flight_time_hrs"],
-        )
         return Response(direct_destinations)
