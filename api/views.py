3ddb5ccff88cca77b52a03a4a32f1c7cf555c88f
Johnny <johnkcli@hotmail.com>
2024-01-30 17:45:56 +0000

    single origin direct destination search

--- api/views.py
+++ api/views.py
@@ -50,90 +50,17 @@ class CitySearchView(APIView):
 
 class DirectDestinationsView(APIView):
     def get(self, request):
-        origin_city_name = request.query_params.get("origin_city_name")
-        origin_country_iata = request.query_params.get("origin_country_iata")
-        origin_city_iata = request.query_params.get("origin_city_iata")
+        city_name = request.query_params.get("city_name")
+        country_iata = request.query_params.get("country_iata")
+        city_iata = request.query_params.get("city_iata")
         token_type, access_token = access_token_and_type()
-        origin_direct_destinations = []
-        if (
-            origin_city_iata
-            and origin_city_name
-            and origin_country_iata
-            and len(origin_country_iata) == 2
-        ):
-            origin_direct_destinations = get_direct_destinations(
-                origin_city_iata,
-                origin_city_name,
-                origin_country_iata,
+        direct_destinations = []
+        if city_iata and city_name and country_iata and len(country_iata) == 2:
+            direct_destinations = get_direct_destinations(
+                city_iata,
+                city_name,
+                country_iata,
                 token_type,
                 access_token,
-            )
-            for destination in origin_direct_destinations:
-                destination["origin_estimated_flight_time_hrs"] = destination.pop(
-                    "estimated_flight_time_hrs"
-                )
-                destination["origin_estimated_flight_time_hrs_mins"] = destination.pop(
-                    "estimated_flight_time_hrs_mins"
-                )
-                destination["destination_estimated_flight_time_hrs"] = None
-                destination["destination_estimated_flight_time_hrs_mins"] = None
-        destination_city_name = request.query_params.get("destination_city_name")
-        destination_country_iata = request.query_params.get("destination_country_iata")
-        destination_city_iata = request.query_params.get("destination_city_iata")
-        destination_direct_destinations = []
-        if (
-            destination_city_iata
-            and destination_city_name
-            and destination_country_iata
-            and len(destination_country_iata) == 2
-        ):
-            destination_direct_destinations = get_direct_destinations(
-                destination_city_iata,
-                destination_city_name,
-                destination_country_iata,
-                token_type,
-                access_token,
-            )
-            for destination in destination_direct_destinations:
-                destination["destination_estimated_flight_time_hrs"] = destination.pop(
-                    "estimated_flight_time_hrs"
-                )
-                destination[
-                    "destination_estimated_flight_time_hrs_mins"
-                ] = destination.pop("estimated_flight_time_hrs_mins")
-                destination["origin_estimated_flight_time_hrs"] = None
-                destination["origin_estimated_flight_time_hrs_mins"] = None
-        if destination_direct_destinations and origin_direct_destinations:
-            direct_destinations = []
-            for origin_destination in origin_direct_destinations:
-                common_destination_index, common_destination = next(
-                    (
-                        (index, destination)
-                        for index, destination in enumerate(
-                            destination_direct_destinations
-                        )
-                        if destination["iataCode"] == origin_destination["iataCode"]
-                    ),
-                    (None, None),
-                )
-                if common_destination:
-                    destination_direct_destinations.pop(common_destination_index)
-                    common_destination[
-                        "origin_estimated_flight_time_hrs"
-                    ] = origin_destination["origin_estimated_flight_time_hrs"]
-                    common_destination[
-                        "origin_estimated_flight_time_hrs_mins"
-                    ] = origin_destination["origin_estimated_flight_time_hrs_mins"]
-                    direct_destinations.append(common_destination)
-            direct_destinations = sorted(
-                direct_destinations,
-                key=lambda destination_city: destination_city[
-                    "origin_estimated_flight_time_hrs"
-                ],
-            )
-
-        else:
-            direct_destinations = (
-                origin_direct_destinations or destination_direct_destinations
             )
         return Response(direct_destinations)
--- functional_tests/test_direct_destinations.py
+++ functional_tests/test_direct_destinations.py
@@ -5,16 +5,13 @@ from django.test import TestCase
 class DirectDestinationsTest(TestCase):
     def test_direct_destinations_only_origin(self):
         response = self.client.get(
-            "/direct-destinations/?destination_city_name=Dubai&"
-            "origin_city_name=London&origin_city_iata=LON&origin_country_iata=GB",
+            "/direct-destinations/?city_name=London&city_iata=LON&country_iata=GB",
             format="json",
         )
         assert response.status_code == HTTPStatus.OK
         assert response.data == sorted(
             response.data,
-            key=lambda destination_city: destination_city[
-                "origin_estimated_flight_time_hrs"
-            ],
+            key=lambda destination_city: destination_city["estimated_flight_time_hrs"],
         )
         assert response.data[0] == {
             "type": "location",
@@ -29,10 +26,8 @@ class DirectDestinationsTest(TestCase):
             },
             "timeZone": {"offSet": "+00:00"},
             "metrics": {"relevance": 4},
-            "origin_estimated_flight_time_hrs": 1.1047879718721132,
-            "origin_estimated_flight_time_hrs_mins": "1h 6m",
-            "destination_estimated_flight_time_hrs": None,
-            "destination_estimated_flight_time_hrs_mins": None,
+            "estimated_flight_time_hrs": 1.1047879718721132,
+            "estimated_flight_time_hrs_mins": "1h 6m",
             "state": None,
             "country": "United Kingdom",
         }
@@ -50,137 +45,16 @@ class DirectDestinationsTest(TestCase):
             },
             "timeZone": {"offSet": "+08:00"},
             "metrics": {"relevance": 13},
-            "origin_estimated_flight_time_hrs": 16.479845565220053,
-            "origin_estimated_flight_time_hrs_mins": "16h 28m",
-            "destination_estimated_flight_time_hrs": None,
-            "destination_estimated_flight_time_hrs_mins": None,
+            "estimated_flight_time_hrs": 16.479845565220053,
+            "estimated_flight_time_hrs_mins": "16h 28m",
             "state": "WA",
             "country": "Australia",
         }
 
-    def test_no_data_for_missing_origin_params(self):
+    def test_no_data_for_missing_params(self):
         response = self.client.get(
-            "/direct-destinations/?origin_city_name=London",
+            "/direct-destinations/?city_name=London",
             format="json",
         )
         assert response.status_code == HTTPStatus.OK
         assert not response.data
-
-    def test_no_data_for_missing_destination_params(self):
-        response = self.client.get(
-            "/direct-destinations/?destination_city_name=London",
-            format="json",
-        )
-        assert response.status_code == HTTPStatus.OK
-        assert not response.data
-
-    def test_direct_destinations_only_destination(self):
-        response = self.client.get(
-            "/direct-destinations/?origin_city_name=Dubai&"
-            "destination_city_name=London&destination_city_iata=LON&destination_country_iata=GB",
-            format="json",
-        )
-        assert response.status_code == HTTPStatus.OK
-        assert response.data == sorted(
-            response.data,
-            key=lambda destination_city: destination_city[
-                "destination_estimated_flight_time_hrs"
-            ],
-        )
-        assert response.data[0] == {
-            "type": "location",
-            "subtype": "city",
-            "name": "Manchester",
-            "iataCode": "MAN",
-            "geoCode": {"latitude": 53.35362, "longitude": -2.275},
-            "address": {
-                "countryName": "UNITED KINGDOM",
-                "countryCode": "GB",
-                "regionCode": "EUROP",
-            },
-            "timeZone": {"offSet": "+00:00"},
-            "metrics": {"relevance": 4},
-            "destination_estimated_flight_time_hrs": 1.1047879718721132,
-            "destination_estimated_flight_time_hrs_mins": "1h 6m",
-            "origin_estimated_flight_time_hrs": None,
-            "origin_estimated_flight_time_hrs_mins": None,
-            "state": None,
-            "country": "United Kingdom",
-        }
-        assert response.data[-1] == {
-            "type": "location",
-            "subtype": "city",
-            "name": "Perth",
-            "iataCode": "PER",
-            "geoCode": {"latitude": -31.94027, "longitude": 115.967},
-            "address": {
-                "countryName": "AUSTRALIA",
-                "countryCode": "AU",
-                "stateCode": "WA",
-                "regionCode": "AUSTL",
-            },
-            "timeZone": {"offSet": "+08:00"},
-            "metrics": {"relevance": 13},
-            "destination_estimated_flight_time_hrs": 16.479845565220053,
-            "destination_estimated_flight_time_hrs_mins": "16h 28m",
-            "origin_estimated_flight_time_hrs": None,
-            "origin_estimated_flight_time_hrs_mins": None,
-            "state": "WA",
-            "country": "Australia",
-        }
-
-    def test_common_direct_destinations(self):
-        response = self.client.get(
-            "/direct-destinations/?origin_city_name=New York&origin_city_iata=NYC&origin_country_iata=US&"
-            "destination_city_name=London&destination_city_iata=LON&destination_country_iata=GB",
-            format="json",
-        )
-        assert response.status_code == HTTPStatus.OK
-        assert len(response.data) == 121
-        assert response.data == sorted(
-            response.data,
-            key=lambda destination_city: destination_city[
-                "origin_estimated_flight_time_hrs"
-            ],
-        )
-        assert response.data[0] == {
-            "type": "location",
-            "subtype": "city",
-            "name": "Boston",
-            "iataCode": "BOS",
-            "geoCode": {"latitude": 42.36528, "longitude": -71.01777},
-            "address": {
-                "countryName": "UNITED STATES OF AMERICA",
-                "countryCode": "US",
-                "stateCode": "MA",
-                "regionCode": "NAMER",
-            },
-            "timeZone": {"offSet": "-05:00"},
-            "metrics": {"relevance": 26},
-            "state": "MA",
-            "country": "United States Of America",
-            "destination_estimated_flight_time_hrs": 6.532059143908586,
-            "destination_estimated_flight_time_hrs_mins": "6h 31m",
-            "origin_estimated_flight_time_hrs": 1.1677705881354619,
-            "origin_estimated_flight_time_hrs_mins": "1h 10m",
-        }
-        assert response.data[-1] == {
-            "type": "location",
-            "subtype": "city",
-            "name": "Singapore",
-            "iataCode": "SIN",
-            "geoCode": {"latitude": 1.35028, "longitude": 103.9945},
-            "address": {
-                "countryName": "SINGAPORE",
-                "countryCode": "SG",
-                "regionCode": "SEASI",
-            },
-            "timeZone": {"offSet": "+08:00"},
-            "metrics": {"relevance": 37},
-            "state": None,
-            "country": "Singapore",
-            "destination_estimated_flight_time_hrs": 12.57693622990517,
-            "destination_estimated_flight_time_hrs_mins": "12h 34m",
-            "origin_estimated_flight_time_hrs": 17.41197832473083,
-            "origin_estimated_flight_time_hrs_mins": "17h 24m",
-        }
